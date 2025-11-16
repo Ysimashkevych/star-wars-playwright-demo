7514b085d2358f19ebadedf581cea9dfb9981d42
Ysimashkevych <yuriy.simashkevych@yahoo.com>
2025-11-16 14:21:33 +0100

    Revert "Initial commit - App"
    This reverts commit 73d65be132523120cf814ea8b3c33d74cf57cdf2.

--- Fedex-QA-Assignment/src/app/components/character/character.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-character',
-  templateUrl: './character.component.html',
-})
-export class CharacterComponent {
-  @Input() character;
-}
--- Fedex-QA-Assignment/src/app/components/planet/planet.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-planet',
-  templateUrl: './planet.component.html',
-})
-export class PlanetComponent {
-  @Input() planet;
-}
